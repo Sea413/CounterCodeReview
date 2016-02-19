293b3bf61aae8420a62de8b4fde335ea20f9aa5b
sea413 <seanmp42@gmail.com>
2016-02-19 09:07:33 -0800

    Allow for preparation of the intial files.

--- Tests/CounterSupatest.cs
+++ Tests/CounterSupatest.cs
@@ -3,44 +3,16 @@ using Xunit;
 using WeekDaynamespace;
 
 
-namespace  WeekDaynamespace
+namespace  CounterNameSpace
 {
-  public class WeekDayTest
+  public class CounterTesting
   {
     [Fact]
-    public void Testforinput_monthfinder_true()
+    public void TestforSentence_CountDracula_sentence()
     {
-      int month = 1;
-      int day = 3;
-      int year = 4000;
+      string sentence = "I have been expecting this test, Mr.Bond"
 
-      Assert.Equal ("Wednesday", WeekDay.monthfinder(month,day,year));
+      Assert.Equal ("I have been expecting this test, Mr.Bond", WordCounter.CountDraculaLoop(sentence));
     }
-    [Fact]
-    public void Testforinput_monthandDayfinder_true()
-    {
-      int month = 9;
-      int day = 3;
-      int year = 1984;
-      Assert.Equal ("Friday", WeekDay.monthfinder(month,day,year));
-    }
-    [Fact]
-    public void Testforinput_Daysarefunnny_Monday()
-    {
-      int month = 2;
-      int day = 15;
-      int year = 2100;
-
-      Assert.Equal ("Wednesday", WeekDay.monthfinder(month,day,year));
-    }
-    [Fact]
-    public void Testforinput_YearsAre365_true()
-    {
-      int month = 2;
-      int day = 15;
-      int year = 2010;
-
-      Assert.Equal ("Wednesday", WeekDay.monthfinder(month,day,year));
     }
   }
-}
